f84a9685c07a7f08503b6d45ca2d3d441158ac1c
Musab HQ <musaab@MacBook-Pro-Musab.local>
2024-07-11 00:57:35 +0300

    fix3

--- components/FAQ.js
+++ components/FAQ.js
@@ -1,31 +1,165 @@
-// "use client";
+// // "use client";
 
-// import { useRef, useState } from "react";
+// // import { useRef, useState } from "react";
 
-// // <FAQ> component is a lsit of <Item> component
-// // Just import the FAQ & add your FAQ content to the const faqList
+// // // <FAQ> component is a lsit of <Item> component
+// // // Just import the FAQ & add your FAQ content to the const faqList
 
-// const faqList = [
-//   {
-//     question: "What do I get exactly?",
-//     answer: <div className="space-y-2 leading-relaxed">Loreum Ipseum</div>,
-//   },
-//   {
-//     question: "Can I get a refund?",
-//     answer: (
-//       <p>
-//         Yes! You can request a refund within 7 days of your purchase. Reach out
-//         by email.
-//       </p>
-//     ),
-//   },
-//   {
-//     question: "I have another question",
-//     answer: (
-//       <div className="space-y-2 leading-relaxed">Cool, contact us by email</div>
-//     ),
-//   },
-// ];
+// // const faqList = [
+// //   {
+// //     question: "What do I get exactly?",
+// //     answer: <div className="space-y-2 leading-relaxed">Loreum Ipseum</div>,
+// //   },
+// //   {
+// //     question: "Can I get a refund?",
+// //     answer: (
+// //       <p>
+// //         Yes! You can request a refund within 7 days of your purchase. Reach out
+// //         by email.
+// //       </p>
+// //     ),
+// //   },
+// //   {
+// //     question: "I have another question",
+// //     answer: (
+// //       <div className="space-y-2 leading-relaxed">Cool, contact us by email</div>
+// //     ),
+// //   },
+// // ];
+
+// // const Item = ({ item }) => {
+// //   const accordion = useRef(null);
+// //   const [isOpen, setIsOpen] = useState(false);
+
+// //   return (
+// //     <li>
+// //       <button
+// //         className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-left border-t md:text-lg border-base-content/10"
+// //         onClick={(e) => {
+// //           e.preventDefault();
+// //           setIsOpen(!isOpen);
+// //         }}
+// //         aria-expanded={isOpen}
+// //       >
+// //         <span
+// //           className={`flex-1 text-base-content ${isOpen ? "text-primary" : ""}`}
+// //         >
+// //           {item?.question}
+// //         </span>
+// //         <svg
+// //           className={`flex-shrink-0 w-4 h-4 ml-auto fill-current`}
+// //           viewBox="0 0 16 16"
+// //           xmlns="http://www.w3.org/2000/svg"
+// //         >
+// //           <rect
+// //             y="7"
+// //             width="16"
+// //             height="2"
+// //             rx="1"
+// //             className={`transform origin-center transition duration-200 ease-out ${
+// //               isOpen && "rotate-180"
+// //             }`}
+// //           />
+// //           <rect
+// //             y="7"
+// //             width="16"
+// //             height="2"
+// //             rx="1"
+// //             className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+// //               isOpen && "rotate-180 hidden"
+// //             }`}
+// //           />
+// //         </svg>
+// //       </button>
+
+// //       <div
+// //         ref={accordion}
+// //         className={`transition-all duration-300 ease-in-out opacity-80 overflow-hidden`}
+// //         style={
+// //           isOpen
+// //             ? { maxHeight: accordion?.current?.scrollHeight, opacity: 1 }
+// //             : { maxHeight: 0, opacity: 0 }
+// //         }
+// //       >
+// //         <div className="pb-5 leading-relaxed">{item?.answer}</div>
+// //       </div>
+// //     </li>
+// //   );
+// // };
+
+// // const FAQ = () => {
+// //   return (
+// //     <section className="bg-base-200" id="faq">
+// //       <div className="py-24 px-8 max-w-7xl mx-auto flex flex-col md:flex-row gap-12">
+// //         <div className="flex flex-col text-left basis-1/2">
+// //           <p className="inline-block font-semibold text-primary mb-4">FAQ</p>
+// //           <p className="sm:text-4xl text-3xl font-extrabold text-base-content">
+// //             Frequently Asked Questions
+// //           </p>
+// //         </div>
+
+// //         <ul className="basis-1/2">
+// //           {faqList.map((item, i) => (
+// //             <Item key={i} item={item} />
+// //           ))}
+// //         </ul>
+// //       </div>
+// //     </section>
+// //   );
+// // };
+
+// // export default FAQ;
+
+// 'use client';
+// import React, { useEffect, useState, useRef } from 'react';
+// import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+// import { Suspense } from 'react';
+// const FAQ = () => {
+//   const [faqList, setFaqList] = useState([]);
+//   const supabase = createClientComponentClient();
+
+//   useEffect(() => {
+//     const fetchFAQ = async () => {
+//       const { data, error } = await supabase
+//         .from('faq')
+//         .select('*')
+//         .order('created_at', { ascending: true });
+
+//       if (error) {
+//         console.error('Error fetching FAQ:', error);
+//       } else {
+//         setFaqList(data);
+//       }
+//     };
+
+//     fetchFAQ();
+//   }, [supabase]);
+
+//   return (
+//     <>
+//     <Suspense>
+
+ 
+//     <section className="bg-base-200 text-right" id="faq">
+//       <div className="py-24 px-8 max-w-7xl mx-auto flex flex-col md:flex-row gap-12 text-right">
+//         <div className="flex flex-col text-left basis-1/2">
+//           <p className="inline-block font-semibold text-primary mb-4">FAQ</p>
+//           <p className="sm:text-4xl text-3xl font-extrabold text-base-content">
+//             Frequently Asked Questions
+//           </p>
+//         </div>
+
+//         <ul className="basis-1/2 text-right">
+//           {faqList.map((item, i) => (
+//             <Item key={i} item={item} />
+//           ))}
+//         </ul>
+//       </div>
+//     </section>
+//     </Suspense>
+//     </>
+//   );
+// };
 
 // const Item = ({ item }) => {
 //   const accordion = useRef(null);
@@ -34,7 +168,7 @@
 //   return (
 //     <li>
 //       <button
-//         className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-left border-t md:text-lg border-base-content/10"
+//         className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-right border-t md:text-lg border-base-content/10"
 //         onClick={(e) => {
 //           e.preventDefault();
 //           setIsOpen(!isOpen);
@@ -42,7 +176,7 @@
 //         aria-expanded={isOpen}
 //       >
 //         <span
-//           className={`flex-1 text-base-content ${isOpen ? "text-primary" : ""}`}
+//           className={`flex-1 text-base-content ${isOpen ? 'text-primary' : ''}`}
 //         >
 //           {item?.question}
 //         </span>
@@ -57,7 +191,7 @@
 //             height="2"
 //             rx="1"
 //             className={`transform origin-center transition duration-200 ease-out ${
-//               isOpen && "rotate-180"
+//               isOpen && 'rotate-180'
 //             }`}
 //           />
 //           <rect
@@ -66,7 +200,7 @@
 //             height="2"
 //             rx="1"
 //             className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-//               isOpen && "rotate-180 hidden"
+//               isOpen && 'rotate-180 hidden'
 //             }`}
 //           />
 //         </svg>
@@ -87,138 +221,4 @@
 //   );
 // };
 
-// const FAQ = () => {
-//   return (
-//     <section className="bg-base-200" id="faq">
-//       <div className="py-24 px-8 max-w-7xl mx-auto flex flex-col md:flex-row gap-12">
-//         <div className="flex flex-col text-left basis-1/2">
-//           <p className="inline-block font-semibold text-primary mb-4">FAQ</p>
-//           <p className="sm:text-4xl text-3xl font-extrabold text-base-content">
-//             Frequently Asked Questions
-//           </p>
-//         </div>
-
-//         <ul className="basis-1/2">
-//           {faqList.map((item, i) => (
-//             <Item key={i} item={item} />
-//           ))}
-//         </ul>
-//       </div>
-//     </section>
-//   );
-// };
-
 // export default FAQ;
-
-'use client';
-import React, { useEffect, useState, useRef } from 'react';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Suspense } from 'react';
-const FAQ = () => {
-  const [faqList, setFaqList] = useState([]);
-  const supabase = createClientComponentClient();
-
-  useEffect(() => {
-    const fetchFAQ = async () => {
-      const { data, error } = await supabase
-        .from('faq')
-        .select('*')
-        .order('created_at', { ascending: true });
-
-      if (error) {
-        console.error('Error fetching FAQ:', error);
-      } else {
-        setFaqList(data);
-      }
-    };
-
-    fetchFAQ();
-  }, [supabase]);
-
-  return (
-    <>
-    <Suspense>
-
- 
-    <section className="bg-base-200 text-right" id="faq">
-      <div className="py-24 px-8 max-w-7xl mx-auto flex flex-col md:flex-row gap-12 text-right">
-        <div className="flex flex-col text-left basis-1/2">
-          <p className="inline-block font-semibold text-primary mb-4">FAQ</p>
-          <p className="sm:text-4xl text-3xl font-extrabold text-base-content">
-            Frequently Asked Questions
-          </p>
-        </div>
-
-        <ul className="basis-1/2 text-right">
-          {faqList.map((item, i) => (
-            <Item key={i} item={item} />
-          ))}
-        </ul>
-      </div>
-    </section>
-    </Suspense>
-    </>
-  );
-};
-
-const Item = ({ item }) => {
-  const accordion = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <li>
-      <button
-        className="relative flex gap-2 items-center w-full py-5 text-base font-semibold text-right border-t md:text-lg border-base-content/10"
-        onClick={(e) => {
-          e.preventDefault();
-          setIsOpen(!isOpen);
-        }}
-        aria-expanded={isOpen}
-      >
-        <span
-          className={`flex-1 text-base-content ${isOpen ? 'text-primary' : ''}`}
-        >
-          {item?.question}
-        </span>
-        <svg
-          className={`flex-shrink-0 w-4 h-4 ml-auto fill-current`}
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center transition duration-200 ease-out ${
-              isOpen && 'rotate-180'
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              isOpen && 'rotate-180 hidden'
-            }`}
-          />
-        </svg>
-      </button>
-
-      <div
-        ref={accordion}
-        className={`transition-all duration-300 ease-in-out opacity-80 overflow-hidden`}
-        style={
-          isOpen
-            ? { maxHeight: accordion?.current?.scrollHeight, opacity: 1 }
-            : { maxHeight: 0, opacity: 0 }
-        }
-      >
-        <div className="pb-5 leading-relaxed">{item?.answer}</div>
-      </div>
-    </li>
-  );
-};
-
-export default FAQ;
