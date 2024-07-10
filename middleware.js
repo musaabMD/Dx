c7b75ece029b5dfaadbaabae07a1494d6e664268
Musab HQ <musaab@MacBook-Pro-Musab.local>
2024-07-11 00:50:13 +0300

    fixing

--- middleware.js
+++ middleware.js
@@ -1,3 +1,14 @@
+// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+// import { NextResponse } from "next/server";
+
+// // The middleware is used to refresh the user's session before loading Server Component routes
+// export async function middleware(req) {
+//   const res = NextResponse.next();
+//   const supabase = createMiddlewareClient({ req, res });
+//   await supabase.auth.getSession();
+//   return res;
+// }
+// middleware.js
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
